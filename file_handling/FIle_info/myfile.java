65b7ae8073b31f35ae70829e98782f28e73b254e
sumitkohli <sumitkohli2004@gmail.com>
2023-09-15 08:32:34 +0530

    file handling

--- file_handling/FIle_info/myfile.java
+++ file_handling/FIle_info/myfile.java
@@ -1,4 +1,5 @@
 package FIle_info;
+
 import java.util.Scanner;
 import java.util.*;
 import java.io.BufferedReader;
@@ -8,24 +9,24 @@ import java.io.IOException;
 
 public class myfile {
 
-    public static void print_string() throws IOException{
-        String path = "C:/Users/DELL/Documents/ColectionFrameWork/file_handling/FIle_info/info.txt";
-        FileWriter fw = new FileWriter(path);
-        Scanner sc = new Scanner(System.in);
+  public static void print_string() throws IOException {
+    String path = "C:/Users/DELL/Documents/ColectionFrameWork/file_handling/FIle_info/info.txt";
+    FileWriter fw = new FileWriter(path);
+    Scanner sc = new Scanner(System.in);
 
-        System.out.println("Eneter a String -");
-        String st= sc.nextLine();
-        
-        while(!st.equals("q")){
-            fw.append(st+"\n");
-            System.out.println("Enter a String - ");
-            st = sc.nextLine();
-        }
+    System.out.println("Eneter a String -");
+    String st = sc.nextLine();
 
-        fw.close();
+    while (!st.equals("q")) {
+      fw.append(st + "\n");
+      System.out.println("Enter a String - ");
+      st = sc.nextLine();
     }
 
-    public static void count_size() throws IOException{
+    fw.close();
+  }
+
+  public static void Get_info() throws IOException {
     String path = "C:/Users/DELL/Documents/ColectionFrameWork/file_handling/FIle_info/info.txt";
     FileReader fr = new FileReader(path);
     BufferedReader br = new BufferedReader(fr);
@@ -33,27 +34,30 @@ public class myfile {
     int wordCount = 0;
     int lineCount = 0;
     String currentLine = "";
-    while((currentLine = br.readLine()) !=null){
+    while ((currentLine = br.readLine()) != null) {
       lineCount++;
-      String [] word = currentLine.split(" ");
-      wordCount+= word.length;
 
-      for(String w : word){
-       charcount+= w.length();
-      } 
+      StringTokenizer word = new StringTokenizer(currentLine);
+            wordCount += word.countTokens();
 
-      System.out.println("Number Of Chars In A File : "+charcount);
-             
-      System.out.println("Number Of Words In A File : "+wordCount);
-       
-      System.out.println("Number Of Lines In A File : "+lineCount);
+      
+        charcount+=currentLine.length();
+      
+
+      
     }
+    System.out.println("Number Of Chars In A File : " + charcount);
 
-     fr.close();
+      System.out.println("Number Of Words In A File : " + wordCount);
 
-    }
-    public static void main(String[] args) throws IOException  {
-      print_string();
-      count_size();
-    }
+      System.out.println("Number Of Lines In A File : " + lineCount);
+
+    fr.close();
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    // print_string();
+    Get_info();
+  }
 }
